4b6916b30ff9a5f3265c85778ee7a0d3d2a426c9
Matias Gomez <matias.gomez.1915@gmail.com>
2023-12-17 18:10:06 -0300

    new commit2

--- js/product.manager.js
+++ js/product.manager.js
@@ -4,25 +4,25 @@ class ProductManager {
 
   createProduct({ title, photo, ...data }) {
     try {
-        if(!title || !photo){
-            throw new Error("Por favor ingrese titulo y foto del producto")
-        }
-        const product = {
-            id:
-              ProductManager.#products.length === 0
-                ? 1
-                : ProductManager.#products[ProductManager.#products.length - 1].id+1,
-            title,
-            photo,
-            price: data.price || 10,
-            stock: data.stock || 50,
-          };
-          ProductManager.#products.push(product);
-          return product;
+      if (!title || !photo) {
+        throw new Error("Por favor ingrese titulo y foto del producto");
+      }
+      const product = {
+        id:
+          ProductManager.#products.length === 0
+            ? 1
+            : ProductManager.#products[ProductManager.#products.length - 1].id +
+              1,
+        title,
+        photo,
+        price: data.price || 10,
+        stock: data.stock || 50,
+      };
+      ProductManager.#products.push(product);
+      return product;
     } catch (error) {
-        return error;       
+      return error.message;
     }
-    
   }
   read() {
     return ProductManager.#products;
@@ -43,18 +43,19 @@ console.log(
 );
 
 console.log(
-    products.createProduct({
-      title: "Playstation",
-      photo:
-        "https://http2.mlstatic.com/D_NQ_NP_951973-MLA70217884926_062023-O.webp",
-    })
-  );
+  products.createProduct({
+    title: "Playstation",
+    photo:
+      "https://http2.mlstatic.com/D_NQ_NP_951973-MLA70217884926_062023-O.webp",
+  })
+);
 
-  console.log(
-    products.createProduct({
-      title: "Dron",
-      photo: "https://http2.mlstatic.com/D_NQ_NP_797396-MLA54056673569_022023-O.webp"
-    })
-  );
+console.log(
+  products.createProduct({
+    title: "Dron",
+    photo:
+      "https://http2.mlstatic.com/D_NQ_NP_797396-MLA54056673569_022023-O.webp",
+  })
+);
 
-console.log(products.readOne(2))
+console.log(products.readOne(2));
