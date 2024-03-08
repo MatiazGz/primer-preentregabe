139ff05649ca538aba0565933b71440602dd9907
Matias Gomez <138533484+MatiazGz@users.noreply.github.com>
2024-03-08 01:03:41 -0300

    integradora

--- src/router/api/index.router.js
+++ src/router/api/index.router.js
@@ -1,35 +1,26 @@
-import { Router } from "express";
 import usersRouter from "./users.router.js";
 import productsRouter from "./products.router.js";
 import ordersRouter from "./orders.router.js";
 import sessionsRouter from "./sessions.router.js";
 import passport from "../../middlewares/passport.mid.js";
-import CustomRouter from '../CustomRouter.js';
-import ClothesRouter from './clothes.router.js';
-import CategoriesRouter from './categories.router.js';
-import SizesRouter  from "./size.router.js";
+import CustomRouter from "../CustomRouter.js";
+import ClothesRouter from "./clothes.router.js";
+import CategoriesRouter from "./categories.router.js";
+import SizesRouter from "./size.router.js";
 
-const apiRouter = Router();
-
-//definir los enrutadores
-apiRouter.use("/users", usersRouter);
-apiRouter.use("/orders", passport.authenticate("jwt", {session : false}) , ordersRouter);
-apiRouter.use("/products", productsRouter);
-apiRouter.use("/sessions", sessionsRouter);
-
-//export default apiRouter;
-//exporto el enrutador de la api para poder implementarlo en el enrutador del servidor
 
 const clothes = new ClothesRouter();
 const categories = new CategoriesRouter();
 const sizes = new SizesRouter();
 
-
 export default class ApiRouter extends CustomRouter {
-    init() {
-      this.use('/sessions', session.getRouter());
-      this.use('/clothes', clothes.getRouter());
-      this.use('/categories', categories.getRouter());
-      this.use("/sizes", sizes.getRouter())
-    }
+  init() {
+    this.use("/products", productsRouter);
+    this.use("/users", usersRouter);
+    this.use("/orders", passport.authenticate("jwt", { session: false }), ordersRouter);
+    this.use("/sessions", sessionsRouter);
+    this.use("/clothes", clothes.getRouter());
+    this.use("/categories", categories.getRouter());
+    this.use("/sizes", sizes.getRouter());
   }
+}
