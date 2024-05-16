72906adc347ec6e1d3bee1ca40927149c2fa217c
MatiazGz <matias.gomez.1915@gmail.com>
2024-05-15 22:02:59 -0300

    new commit

---
+++ test/mocha/product.test.js
@@ -0,0 +1,12 @@
+import assert from "assert"
+import "dotenv/config.js"
+import dao from "../../src/data/index.factory.js"
+
+const { products } = dao
+
+describe(
+    "Testing product model",
+    ()=>{
+        const model = 
+    }
+)
